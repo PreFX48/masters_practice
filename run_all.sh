f274182ea0ff2a14cea9813ab4587c95c02fef0f
Vitaly Sopov <v-sopov@yandex-team.ru>
2020-09-27 18:07:32 +0300

    Update script to wrap commands in shell

--- run_all.sh
+++ run_all.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/bash
 
-sbatch -c 8 --gpus=1 amazon.py
-sbatch -c 8 --gpus=1 amazon_ctr.py
-sbatch -c 8 --gpus=1 amazon_ctr_2.py
-sbatch -c 8 --gpus=1 amazon_ctr_255.py
-sbatch -c 8 --gpus=1 adult.py
-sbatch -c 8 --gpus=1 adult_ctr.py
-sbatch -c 8 --gpus=1 appetency.py
-sbatch -c 8 --gpus=1 appetency_ctr.py
-sbatch -c 8 --gpus=1 upselling.py
-sbatch -c 8 --gpus=1 upselling_ctr.py
-sbatch -c 8 --gpus=1 kick.py
-sbatch -c 8 --gpus=1 kick_ctr.py
-sbatch -c 8 --gpus=1 higgs.py
-sbatch -c 8 --gpus=1 higgs_ctr.py
+sbatch -c 8 --gpus=1 --wrap="python amazon.py"
+sbatch -c 8 --gpus=1 --wrap="python amazon_ctr.py"
+sbatch -c 8 --gpus=1 --wrap="python amazon_ctr_2.py"
+sbatch -c 8 --gpus=1 --wrap="python amazon_ctr_255.py"
+sbatch -c 8 --gpus=1 --wrap="python adult.py"
+sbatch -c 8 --gpus=1 --wrap="python adult_ctr.py"
+sbatch -c 8 --gpus=1 --wrap="python appetency.py"
+sbatch -c 8 --gpus=1 --wrap="python appetency_ctr.py"
+sbatch -c 8 --gpus=1 --wrap="python upselling.py"
+sbatch -c 8 --gpus=1 --wrap="python upselling_ctr.py"
+sbatch -c 8 --gpus=1 --wrap="python kick.py"
+sbatch -c 8 --gpus=1 --wrap="python kick_ctr.py"
+sbatch -c 8 --gpus=1 --wrap="python higgs.py"
+sbatch -c 8 --gpus=1 --wrap="python higgs_ctr.py"
